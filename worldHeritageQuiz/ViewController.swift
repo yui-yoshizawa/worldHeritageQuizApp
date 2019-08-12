8f652119d94aa30f05bf13a826f8bed15d3dc8b5
yui-yoshizawa <yui.yossy.0114@gmail.com>
2019-08-12 11:12:27 +0900

    firstCommit

---
+++ worldHeritageQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  worldHeritageQuiz
+//
+//  Created by 吉澤優衣 on 2019/08/12.
+//  Copyright © 2019 吉澤優衣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
